67207a81f3f1fa504780b56f6dbd31124c71cc2e
sandeepkumaar.c <cskumaar1992@gmail.com>
2024-06-11 17:39:59 +0530

    theme and readme

--- src/superhero-table/index.jsx
+++ src/superhero-table/index.jsx
@@ -38,33 +38,33 @@ export default function SuperHeroTable() {
 
 
   const columns = [
-    {
-      id: "select",
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && "indeterminate")
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => {
-            row.toggleSelected(!!value)
-            console.log('row', row.original);
-          }}
-          aria-label="Select row"
-        />
-      ),
-    },
-    {
-      accessorKey: "id",
-      header: "id",
-    },
+    //{
+    //  id: "select",
+    //  header: ({ table }) => (
+    //    <Checkbox
+    //      checked={
+    //        table.getIsAllPageRowsSelected() ||
+    //        (table.getIsSomePageRowsSelected() && "indeterminate")
+    //      }
+    //      onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+    //      aria-label="Select all"
+    //    />
+    //  ),
+    //  cell: ({ row }) => (
+    //    <Checkbox
+    //      checked={row.getIsSelected()}
+    //      onCheckedChange={(value) => {
+    //        row.toggleSelected(!!value)
+    //        console.log('row', row.original);
+    //      }}
+    //      aria-label="Select row"
+    //    />
+    //  ),
+    //},
+    //{
+    //  accessorKey: "id",
+    //  header: "id",
+    //},
     {
       accessorKey: "name",
       header: "name",
